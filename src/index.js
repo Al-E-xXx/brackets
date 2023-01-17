ca749e046b7cc932a632363d298a227f243d100d
Alex <asgaroth@yandex.ru>
2023-01-17 08:02:15 +0300

    feat: Complete my task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = '';
+  let coincidence = false;
+  
+  main_loop: for (let i = 0; i < str.length; i++) {
+    for (let n = 0; n < bracketsConfig.length; n++) {
+      if (str[i] === bracketsConfig[n][0] && str[i + 1] === bracketsConfig[n][1]) {
+        newStr = str.slice(0, i) + str.slice(i + 2);      
+        // console.log(newStr);
+        coincidence = true;
+        break main_loop;
+      } else {
+        newStr = str;
+      }
+    }
+  }    
+
+  if (coincidence === true && newStr.length > 0) {
+    return check(newStr, bracketsConfig);
+  } else {
+    if (newStr.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }  
 }
