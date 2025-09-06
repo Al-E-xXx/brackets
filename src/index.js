67911db2b7ac0ae94663f8da15b9929e990ca102
alex <vurdalackoff@gmail.com>
2025-09-06 21:17:44 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  let newStr = '';
+  let coincidence = false;
+
+  for (let i = 0; i < str.length; i += 1) {
+    let found = false;
+
+    for (let n = 0; n < bracketsConfig.length; n += 1) {
+      if (
+        str[i] === bracketsConfig[n][0] &&
+        str[i + 1] === bracketsConfig[n][1]
+      ) {
+        newStr = str.slice(0, i) + str.slice(i + 2);
+        coincidence = true;
+        found = true;
+        break;
+      }
+    }
+
+    if (found) {
+      break;
+    } else {
+      newStr = str;
+    }
+  }
+
+  if (coincidence && newStr.length > 0) {
+    return check(newStr, bracketsConfig);
+  }
+
+  return newStr.length === 0;
 };
